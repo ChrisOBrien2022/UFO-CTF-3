a7d44102586ebcbe18d95e91f02a0660f9839f9f
Chris O'Brien <108006132+VanDunks@users.noreply.github.com>
2024-03-26 23:05:40 +0200

    I'm tired

---
+++ Assets/References.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class References : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/*
+OpenAI. (2024). ChatGPT (March 26 version) [Large language model]. https://chat.openai.com
+Noblob. (2022) “Picking up and Carrying Objects: Unity 3D.” YouTube. youtu.be/jqbrq-EIypk?si=yqKYFG6BWKAEmTIk. 
+/*
